b77539d4b80f6dbdf61a88b81b7b23f0eb6cd184
Hektor <hcostaguzman@gmail.com>
2021-01-03 03:21:55 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-895a6c0da5d4efd1ac07.js"
+    "url": "webpack-runtime-cae67eca8d2b2427e7d9.js"
   },
   {
     "url": "styles.8ee71e7cce2bdd44a725.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "204db62871faa4b268f69b3c86ce8b43"
+    "revision": "37c94f212c3f2fbf40a19c5312664105"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "45dce618cef9de96b46f2739d746822e"
+    "revision": "adb9ba57f684fd85ec5c39b3bcd8cedc"
   },
   {
     "url": "polyfill-f33cc994945b2249d5ee.js"
